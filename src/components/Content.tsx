311f3450a81a24d126bda9d90525038a80426223
Ewerton <ewerton2004@yahoo.com.br>
2022-11-22 14:46:56 -0300

    Section 'Populares da Semana' made

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,9 @@
+import ArtCard from "./card/ArtCard"
+
+const astronaut1 = require('../assets/astronauta-1.png')
+const astronaut2 = require('../assets/astronauta-2.png')
+const astronaut3 = require('../assets/astronauta-3.png')
+const astronaut4 = require('../assets/astronauta-4.png')
 const banner = require('../assets/banner.png')
 
 export default function Content() {
@@ -30,6 +36,19 @@ export default function Content() {
             </div>
           </div>
           <div><img src={banner} alt="Banner de um homem usando um óculos diferente" /></div>
+        </div>
+      </section>
+      <section className="max-w-7xl flex-col w-full pb-16">
+        <h2 className="text-5xl font-bold py-40"><span className="logo-color">Populares</span> da Semana</h2>
+        <div className="flex items-center justify-between">
+          <div className="flex flex-col gap-12 pr-68 gray-border-right">
+            <ArtCard astronaut="Astronauta 1" name="Mayk Brito" price={1.50} src={astronaut1} alt='Imagem do astronauta 1'/>
+            <ArtCard astronaut="Astronauta 2" name="Jakeliny Gracielly" price={2.0} src={astronaut2} alt='Imagem do astronauta 2'/>
+          </div>
+          <div className="flex flex-col gap-12">
+            <ArtCard astronaut="Astronauta 3" name="João Inácio (Biro)" price={3.75} src={astronaut3} alt='Imagem do astronauta 3'/>
+            <ArtCard astronaut="Astronauta 4" name="Tamires Santos" price={4.30} src={astronaut4} alt='Imagem do astronauta 4'/>
+          </div>
         </div>
       </section>
     </div>
--- src/components/card/ArtCard.tsx
+++ src/components/card/ArtCard.tsx
@@ -10,13 +10,14 @@ interface ArtCardProps {
 
 export default function ArtCard(props: ArtCardProps) {
   return (
-    <div>
-      <div>
-        <h2>{props.astronaut}</h2>
-        <img src={topRightArrow} alt="seta mirando para o canto superior direito" />
+    <div className="flex flex-col gap-6 mr-3">
+      <div className="flex justify-between pb-5">
+        <h2 className="text-3xl text-white font-medium">{props.astronaut}</h2>
+        <img src={topRightArrow} alt="seta mirando para o canto superior direito" width={25} height={25} />
       </div>
-      <div>
-        <span>{props.name}</span><span className="logo-color">{props.price} RKT</span>
+      <div className="flex justify-between text-xl">
+        <span className="text-gray-subtitle font-semibold">{props.name}</span>
+        <span className="logo-color">{props.price} RKT</span>
       </div>
       <img src={props.src} alt={props.alt} />
     </div>
