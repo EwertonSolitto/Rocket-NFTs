0591184a956ea8212e178fb4038536a10c74e532
Ewerton <ewerton2004@yahoo.com.br>
2022-11-22 23:50:51 -0300

    made ArtCard animations

--- src/components/card/ArtCard.tsx
+++ src/components/card/ArtCard.tsx
@@ -10,7 +10,7 @@ interface ArtCardProps {
 
 export default function ArtCard(props: ArtCardProps) {
   return (
-    <div className="flex flex-col gap-6 mr-3">
+    <a href="/" className="flex flex-col gap-6 mr-3 hover:scale-110 ease-out duration-500">
       <div className="flex justify-between pb-5">
         <h2 className="text-3xl text-white font-medium">{props.astronaut}</h2>
         <img src={topRightWhiteArrow} alt="Seta branca mirando para o canto superior direito" width={25} height={25} />
@@ -20,6 +20,6 @@ export default function ArtCard(props: ArtCardProps) {
         <span className="text-rose-500">{props.price} RKT</span>
       </div>
       <img src={props.src} alt={props.alt} />
-    </div>
+    </a>
   )
 }
