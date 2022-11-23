8a212854de1abd690e85c0df8d48e13fe515ba9c
Ewerton <ewerton2004@yahoo.com.br>
2022-11-22 22:51:35 -0300

    Colors & font configured on tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
   content: ['./src/**/*.tsx'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        stone: {
+          900: '#0e0e0e'
+        },
+        neutral: {
+          600: '#5a5757',
+          800: '#252525',
+          900: '#0D0D0D'
+        },
+        rose: {
+          500: '#ff5b50'
+        }
+      }
+    },
+    fontFamily: {
+      sans: ['Montserrat', 'sans-serif']
+    }
   },
   plugins: []
 }
