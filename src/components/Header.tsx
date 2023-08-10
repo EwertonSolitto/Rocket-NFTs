3e56875b71be097d45cd6c8683137e307be55432
Ewerton <ewerton2004@yahoo.com.br>
2023-08-09 21:50:33 -0300

    Create the menu on header mobile

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,19 @@
+import { useState } from "react"
 import CloseMenu from "./svgs/CloseMenu"
 import OpenMenu from "./svgs/OpenMenu"
 import MetaMaskLogo from "./svgs/logos/MetaMaskLogo"
 import RocketBlogLogo from "./svgs/logos/RocketBlogLogo"
 
 export default function Header() {
+  const [menu, setMenu] = useState('hidden')
+
   return (
-    <header className="grid items-center justify-center md:grid-cols-3 grid-cols-2 md:pt-14 pt-7 h-28 md:px-0 px-3 md:max-w-none max-w-lg">
+    <header className={`grid items-center justify-center md:grid-cols-3 grid-cols-2 md:pt-14 pt-7 h-28 md:px-0 px-3 md:min-w-0 min-w-full ${menu === 'hidden' ? '' : 'mb-16'}`}>
       <div className="flex items-center">
         <a href="/" id="header-logo"><RocketBlogLogo width={265} height={43}/></a>
       </div>
-      <nav className="hidden items-center justify-between m-auto md:flex">
-        <ul className="md:flex items-center justify-between xl:gap-16 lg:gap-8 md:gap-4 text-white text-lg">
+      <nav className={`items-center justify-between m-auto md:flex ${menu} top-28 right-3 mb:p-0 p-2 md:border-0 border-2 border-neutral-500`}>
+        <ul className="md:flex items-center justify-between xl:gap-16 lg:gap-8 md:gap-4 text-white mb:text-lg">
           <li><a href="/" className="underline-offset-8 focus:underline hover:underline">Comprar NFT</a></li>
           <li><a href="/" className="underline-offset-8 focus:underline hover:underline">Sobre</a></li>
           <li><a href="/" className="underline-offset-8 focus:underline hover:underline">FAQ</a></li>
@@ -21,10 +24,10 @@ export default function Header() {
         <span className="flex items-center font-bold lg:pl-3 pl-2">Conectar carteira</span>
       </button>
       <div className="md:hidden ml-auto">
-        <button>
+        <button onClick={() => {setMenu(() => {return 'absolute'})}} className={menu === 'hidden' ? "block" : 'hidden'}>
           <OpenMenu />
         </button>
-        <button className="hidden">
+        <button onClick={() => {setMenu(() => {return 'hidden'})}} className={menu === 'hidden' ? "hidden" : 'block'}>
           <CloseMenu />
         </button>
       </div>
